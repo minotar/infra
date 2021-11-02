086707cc6fd1c7f9f856c100077bb1a8e3772d05
Luke Hanley <luke.handle@googlemail.com>
2021-11-02 14:23:10 +0000

    Terraform: monitoring node domains + FW

--- terraform/cluster.minotar.net.tf
+++ terraform/cluster.minotar.net.tf
@@ -2,15 +2,32 @@ resource "digitalocean_domain" "net_minotar_cluster" {
   name       = "cluster.minotar.net"
 }
 
-resource "digitalocean_record" "net_minotar_cluster_monitoring" {
-  for_each = tomap({
-    "A" = digitalocean_droplet.monitoring.ipv4_address,
-    "AAAA" = digitalocean_droplet.monitoring.ipv6_address,
-  })
+resource "digitalocean_record" "net_minotar_cluster_monitoring_A" {
+  for_each = toset([
+    "monitoring",
+    "auth",
+    "prometheus.monitoring",
+    "grafana.monitoring",
+  ])
 
   domain = digitalocean_domain.net_minotar_cluster.name
-  type   = each.key
-  name   = "monitoring"
-  value  = each.value
+  type   = "A"
+  name   = each.value
+  value  = digitalocean_droplet.monitoring.ipv4_address
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "net_minotar_cluster_monitoring_AAAA" {
+  for_each = toset([
+    "monitoring",
+    "auth",
+    "prometheus.monitoring",
+    "grafana.monitoring",
+  ])
+
+  domain = digitalocean_domain.net_minotar_cluster.name
+  type   = "AAAA"
+  name   = each.value
+  value  = digitalocean_droplet.monitoring.ipv6_address
   ttl    = 3600
 }
--- terraform/monitoring.minotar.net.tf
+++ terraform/monitoring.minotar.net.tf
@@ -16,3 +16,49 @@ resource "digitalocean_droplet" "monitoring" {
   tags      = [digitalocean_tag.monitoring_nodes.id]
   user_data = data.template_file.user_data.rendered
 }
+
+resource "digitalocean_firewall" "minotar_monitoring_public" {
+  name = "public-monitoring"
+
+  tags = [digitalocean_tag.monitoring_nodes.id]
+
+  // INBOUND
+
+  // SSH / HTTP / HTTPS
+  dynamic "inbound_rule" {
+    for_each = toset([
+      "22",
+      "80",
+      "443",
+    ])
+    content {
+      protocol         = "tcp"
+      port_range       = inbound_rule.value
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    }
+  }
+
+  // ICMP Ping
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  // OUTBOUND
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+}
